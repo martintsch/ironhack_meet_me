8ae89b7db4d1882803ca4e3707b82871a36e2ad9
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 17:48:23 +0200

    Adding locations show with template EX: 17

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,8 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.last_created(10)
   end
+
+  def show
+    @location = Location.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'contact' => 'home#contact'
 
   get 'locations' => 'locations#index'
+  get 'locations/:id' => 'locations#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
