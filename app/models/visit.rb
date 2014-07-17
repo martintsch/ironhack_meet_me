558841326c14459eaf2ece92e3de1be4b748f27c
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 21:39:49 +0200

    15 model validation EX 27b

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -5,4 +5,12 @@ class Visit < ActiveRecord::Base
   validates :from_date, presence: true
   validates :to_date, presence: true
   validates :location_id, presence: true
+
+  validate :from_date_is_before_to_date
+
+  def from_date_is_before_to_date
+    if from_date.to_i > to_date.to_i
+      errors.add(:from_date, "can't be after to date")
+    end
+  end
 end
