306ab6daf010e98f8e40e139a153efba1dff590f
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 17:20:43 +0200

    Playing with Locations 06-CRUD-CR Exercise 12

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ActiveRecord::Base
+  def self.iron_find(id)
+    where(id: id).first
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "finds the Location just like the find method" do
+    location = Location.create name: "Martin"
+
+    found_location = Location.find location.id
+    iron_found_location = Location.iron_find location.id
+
+    expect(found_location).to eq(iron_found_location)
+  end
 end
