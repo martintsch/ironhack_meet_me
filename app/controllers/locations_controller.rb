4f6f63231bf293cc394548ddc73c0385f7f55539
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 18:02:52 +0200

    Adding no locations template Ex 19

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,7 @@
 class LocationsController < ApplicationController
   def index
     @locations = Location.last_created(10)
+    render 'no_locations', status: 404 unless @locations.any?
   end
 
   def show
