79280f12ad7453e3074975d0481c9e4285c93482
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 22:01:00 +0200

    18 paths and urls EX 28

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -7,7 +7,27 @@ class VisitsController < ApplicationController
   def show
   end
 
+  def new
+    @location = Location.find(params[:location_id])
+    @visit = @location.visits.new
+  end
+
+  def create
+    @location = Location.find(params[:location_id])
+    @visit = @location.visits.new visit_params
+
+    if @visit.save
+      redirect_to action: 'index', controller: 'visits', location_id: @location.id
+    else
+      render 'new'
+    end
+  end
+
   private
+
+  def visit_params
+    params.require(:visit).permit(:user_name, :from_date, :to_date)
+  end
 
   def today
     Date.today
