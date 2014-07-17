a5ceea0ec8a3a4b6fcc722e1a3c5ffb714c3c2fd
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 17:02:15 +0200

    03-design-pattern-MVC Exercise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home' => 'home#welcome'
+  get 'contact' => 'home#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
