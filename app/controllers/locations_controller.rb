53cb296cc58b2da2bc393039aba448f12302c5bd
Martin Tschischauskas <martin.tschischauskas@googlemail.com>
2014-07-17 17:35:35 +0200

    Adding locations controller 09-requesting data Ex 16

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,5 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.order(created_at: :desc)
-                         .limit(10)
+    @locations = Location.last_created(10)
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,6 @@
 class Location < ActiveRecord::Base
+  scope :last_created, ->(limit) { order(created_at: :desc).limit(limit) }
+
   def self.iron_find(id)
     where(id: id).first
   end
